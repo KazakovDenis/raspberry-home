a581f9b8f7c82b4aeac2c624ec378c34c0b9616c
Denis Kazakov <denis@kazakov.ru.net>
2022-05-29 23:18:04 +0300

    merge video & audio streams

--- motion/on_movie_end.sh
+++ motion/on_movie_end.sh
@@ -8,7 +8,13 @@ ARECORD_PID=$(cat "$ARECORD_PID_FILE")
 kill -s SIGINT "$ARECORD_PID"
 echo "Audio recording stopped: $1"
 
-# TODO
-# 1. Merge video & audio
-# 2. Move to the target directory
-# 3. Remove source files
+echo "Merging video & audio: $1"
+MERGED="$1.merged.mkv"
+ffmpeg -i "$1" -i "$1.wav" -c copy "$MERGED"
+echo "Merged: $1"
+
+# echo "Moving result: $1"
+# mv "$MERGED" .
+
+echo "Removing sources: $1"
+rm "$1" "$1.wav"

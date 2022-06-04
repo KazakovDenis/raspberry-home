f60c12af5062be9e2eebead59cf76ff6b795cf76
Denis Kazakov <denis@kazakov.ru.net>
2022-06-05 00:18:01 +0300

    add auth, commands & logging for command agent

--- control/cmd_agent/agent.py
+++ control/cmd_agent/agent.py
@@ -6,8 +6,9 @@ Pure standard Python HTTP server to match HTTP requests to system commands in on
     $ sudo systemctl enable cmd_agent.service
     $ sudo systemctl start cmd_agent.service
 3. [Optional] Generate certificates to enable SSL:
-    $ mkdir -p /opt/agent
-    $ openssl req -x509 -newkey rsa:2048 -keyout /opt/agent/key.pem -out /opt/agent/cert.pem -days 365
+    $ mkdir -p /opt/cmd_agent
+    $ openssl req -x509 -newkey rsa:2048 -keyout /opt/cmd_agent/key.pem -out /opt/cmd_agent/cert.pem -days 365
+3.1. Enable SSL setting ENABLE_SSL=True
 """
 import json
 import logging
@@ -18,23 +19,30 @@ import subprocess
 from datetime import datetime
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from json import JSONDecodeError
+from pathlib import Path
 from typing import Tuple
 
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+)
 
+AUTH_TOKEN = os.getenv('AUTH_TOKEN')
 ENABLE_SSL = os.getenv('ENABLE_SSL')
-ADDRESS = ('0.0.0.0', 443 if ENABLE_SSL else 8000)
-CERT_KEY = '/opt/agent/key.pem'
-CERT_PATH = '/opt/agent/cert.pem'
+
+ADDRESS = ('0.0.0.0', 443 if ENABLE_SSL else 80)
+CERT_KEY = Path('/opt/cmd_agent/key.pem')
+CERT_PATH = Path('/opt/cmd_agent/cert.pem')
 
 
-def on_signal(*args):
-    logging.info('Shutting down the server.')
+def shutdown(sig_num, frame):
+    sig = signal.Signals(sig_num).name
+    logging.info('%s received. Shutting down.', sig)
     raise KeyboardInterrupt
 
 
-class BadRequest(Exception):
+class ClientError(Exception):
     pass
 
 
@@ -47,13 +55,15 @@ class Command:
 class Shutdown(Command):
 
     def exec(self) -> Tuple[int, bytes]:
-        pass
+        result = subprocess.run(['sudo', 'shutdown', '+1'])
+        return (201, b'SCHEDULED') if result.returncode == 0 else (500, b'ERROR')
 
 
 class Reboot(Command):
 
     def exec(self) -> Tuple[int, bytes]:
-        pass
+        result = subprocess.run(['sudo', 'shutdown', '-r', '+1'])
+        return (201, b'SCHEDULED') if result.returncode == 0 else (500, b'ERROR')
 
 
 class TestCommand(Command):
@@ -89,39 +99,51 @@ class Handler(BaseHTTPRequestHandler):
         try:
             cmd = self._validate()
             code, msg = commands[cmd].exec()
-        except BadRequest as e:
-            self.send_response(*e.args)
-        except Exception:
+        except ClientError as e:
+            self.send_response(e.args[0])
+            self.wfile.write(e.args[1])
+        except Exception:   # noqa
+            logging.exception('Error during request.')
             self.send_response(500)
         else:
             self.send_response(code)
             self.wfile.write(msg)
 
     def _validate(self) -> str:
+        body = b''
         try:
             content_length = int(self.headers['Content-Length'])
-            body = self.rfile.read(content_length)
-            command = json.loads(body)['command']
-        except (TypeError, ValueError, KeyError, JSONDecodeError):
-            raise BadRequest(400, 'Bad request')
+            body = json.loads(self.rfile.read(content_length))
+            self._auth(body.get('token'))
+            command = body.get('command')
+        except (AttributeError, TypeError, ValueError, JSONDecodeError):
+            logging.warning('Bad request: %s', body)
+            raise ClientError(400, b'Bad request')
         if command not in commands:
-            raise BadRequest(403, 'Forbidden')
+            logging.warning('Forbidden: %s', body)
+            raise ClientError(403, b'Forbidden')
         return command
+
+    @staticmethod
+    def _auth(token: str):
+        if AUTH_TOKEN and token != AUTH_TOKEN:
+            raise ClientError(401, b'Unauthorized')
 
 
 if __name__ == '__main__':
-    signal.signal(signal.SIGINT, on_signal)
-    signal.signal(signal.SIGTERM, on_signal)
+    signal.signal(signal.SIGINT, shutdown)
+    signal.signal(signal.SIGTERM, shutdown)
 
     httpd = HTTPServer(ADDRESS, Handler)
     if ENABLE_SSL:
+        logging.info('SSL enabled.')
         httpd.socket = ssl.wrap_socket(
             sock=httpd.socket,
             keyfile=CERT_KEY,
             certfile=CERT_PATH,
             server_side=True,
         )
-    logging.info('Serving on %s:%s', *ADDRESS)
+    logging.info('Serving on %s:%s.', *ADDRESS)
     try:
         httpd.serve_forever()
     except KeyboardInterrupt:

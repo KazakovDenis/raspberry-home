9326cb40c53cff2a7e83b30102d6a51a44c16788
Denis Kazakov <denis@kazakov.ru.net>
2022-06-11 00:27:25 +0300

    add bot docker image

--- control/bot/bot.py
+++ control/bot/bot.py
@@ -1,9 +1,28 @@
+"""The bot to control Raspberry Pi & other units remotely.
+
+Available commands:
+    pi_shutdown - Shutdown Raspberry Pi
+    pi_reboot - Reboot Raspberry Pi
+    pi_temp - Get Raspberry Pi temperature
+    motion_restart - Restart video monitoring with Motion
+    motion_quit - Stop video monitoring with Motion
+    motion_event_start - Start a Motion event
+    motion_event_stop - Stop a Motion event
+"""
+import logging
 import os
 
 from aiogram import Bot, Dispatcher, executor, types
 
 
-API_TOKEN = os.getenv('BOT_API_TOKEN', '<TOKEN>')
+API_TOKEN = os.getenv('BOT_API_TOKEN')
+OWNER_ID = int(os.getenv('BOT_OWNER_ID', 0))
+assert API_TOKEN, 'No token specified'
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+)
 
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
@@ -11,6 +30,8 @@ dp = Dispatcher(bot)
 
 @dp.message_handler(commands=['start', 'help'])
 async def send_welcome(message: types.Message):
+    if message.from_user != OWNER_ID:
+        await message.reply("Sorry, you can't send messages to this bot.")
     await message.reply('Hello!')
 
 

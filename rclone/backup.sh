918f55d5a64d89a6cfce46444b79c6055610fe4e
Denis Kazakov <denis@kazakov.ru.net>
2022-12-23 01:18:36 +0300

    export logs for non-dockerized tools

--- rclone/backup.sh
+++ rclone/backup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 set -eu
-echo $(date)
+echo "$(date)"
 
 BACKUP_SOURCE_DIR=/media/share
 IGNORE_FILE=ignore.txt
@@ -12,14 +12,17 @@ BACKUP_SECRET_DST=secret:
 BACKUP_OPEN_SRC="public"
 BACKUP_OPEN_DST=remote:backup
 
+LOG_FILE=/var/log/rclone-backup.log
+LOG_LEVEL=INFO
+
 for i in $BACKUP_SECRET_SRC; do
   echo "Synchronizing $i..."
-  rclone sync --exclude-from $IGNORE_FILE --progress $BACKUP_SOURCE_DIR/$i $BACKUP_SECRET_DST/$i
+  rclone --log-file $LOG_FILE --log-level $LOG_LEVEL sync --exclude-from $IGNORE_FILE --progress $BACKUP_SOURCE_DIR/$i $BACKUP_SECRET_DST/$i
 done
 
 for i in $BACKUP_OPEN_SRC; do
   echo "Synchronizing $i..."
-  rclone sync --exclude-from $IGNORE_FILE --progress $BACKUP_SOURCE_DIR/$i $BACKUP_OPEN_DST/$i
+  rclone --log-file $LOG_FILE --log-level $LOG_LEVEL sync --exclude-from $IGNORE_FILE --progress $BACKUP_SOURCE_DIR/$i $BACKUP_OPEN_DST/$i
 done
 
 echo "Done!"
